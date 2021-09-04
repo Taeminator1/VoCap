876ba4ee777666553ab04ed3eb09883db1cbca5a
Taemin <dbsxoals689@naver.com>
2021-09-04 22:02:53 +0900

    Pod 파일 재설치 및 PersonalInfo 수정

--- VoCap/User Interface/Helper/AdMobView.swift
+++ VoCap/User Interface/Helper/AdMobView.swift
@@ -9,15 +9,13 @@ import SwiftUI
 import UIKit
 import GoogleMobileAds
 
-// Test baner adUnitID: "ca-app-pub-3940256099942544/2934735716"
-// VoCap baner adUnitID: "ca-app-pub-1173596179673040/5837435794"
 struct GADBannerViewController: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
         let view = GADBannerView(adSize: kGADAdSizeBanner)
         let viewController = UIViewController()
+
         
-//        view.adUnitID = "ca-app-pub-3940256099942544/2934735716"            // for Test
-        view.adUnitID = "ca-app-pub-1173596179673040/5837435794"            // for Release
+        view.adUnitID = PersonalInfo.adUnitID
         view.rootViewController = viewController
         viewController.view.addSubview(view)
         viewController.view.frame = CGRect(origin: .zero, size: kGADAdSizeBanner.size)
@@ -30,16 +28,3 @@ struct GADBannerViewController: UIViewControllerRepresentable {
         
     }
 }
-
-//struct AdMobView: View {
-//    var body: some View {
-//        GADBannerViewController()
-//            .frame(width: kGADAdSizeBanner.size.width, height: kGADAdSizeBanner.size.height)
-//    }
-//}
-//
-//struct AdMobView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        AdMobView()
-//    }
-//}
