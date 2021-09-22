7697e96699f48989bbc4d8e190a717a160e5e954
Taemin <dbsxoals689@naver.com>
2021-09-22 22:07:57 +0900

    CustomSheetDismiss 리팩터링

--- VoCap/User Interface/Helper/Custom Object/CustomSheetDismiss.swift
+++ VoCap/User Interface/Helper/Custom Object/CustomSheetDismiss.swift
@@ -1,28 +1,26 @@
 //
-//  Created by https://quickplan.app on 2020/11/8.
+//  CustomSheetDismiss.swift
+//  VoCap
 //
+//  Created by https://quickplan.app on 2020/11/8.
+//  Modified by Taeminator on 2021/3/26.
+//
+
+//  Control if allow to dismiss the sheet by the user actions:
+//  - Drag down on the sheet.
+//  - Tap outside the sheet
+
+//  Reference: https://gist.github.com/mobilinked/9b6086b3760bcf1e5432932dad0813c0
 
 import SwiftUI
 
-/// Control if allow to dismiss the sheet by the user actions
-/// - Drag down on the sheet on iPhone and iPad
-/// - Tap outside the sheet on iPad
-/// No impact to dismiss programatically (by calling "presentationMode.wrappedValue.dismiss()")
-/// -----------------
-/// Tested on iOS 14.2 with Xcode 12.2 RC
-/// This solution may NOT work in the furture.
-/// -----------------
-
-//  https://gist.github.com/mobilinked/9b6086b3760bcf1e5432932dad0813c0
-//  edited by Taeminator1
-
 struct MbModalHackView: UIViewControllerRepresentable {
     var dismissable: () -> Bool = { false }
-    @Binding var showingCancelSheet: Bool
+    @Binding var isPresented: Bool
     @Binding var isSending: Bool
     
     func makeUIViewController(context: UIViewControllerRepresentableContext<MbModalHackView>) -> UIViewController {
-        MbModalViewController(dismissable: self.dismissable, showingCancelSheet: $showingCancelSheet, isSending: $isSending)
+        MbModalViewController(dismissable: self.dismissable, isPresented: $isPresented, isSending: $isSending)
     }
     
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
@@ -33,12 +31,12 @@ struct MbModalHackView: UIViewControllerRepresentable {
 extension MbModalHackView {
     private final class MbModalViewController: UIViewController, UIAdaptivePresentationControllerDelegate {
         let dismissable: () -> Bool
-        @Binding var showingCancelSheet: Bool
+        @Binding var isPresented: Bool
         @Binding var isSending: Bool
         
-        init(dismissable: @escaping () -> Bool, showingCancelSheet: Binding<Bool>, isSending: Binding<Bool>) {
+        init(dismissable: @escaping () -> Bool, isPresented: Binding<Bool>, isSending: Binding<Bool>) {
             self.dismissable = dismissable
-            _showingCancelSheet = showingCancelSheet
+            _isPresented = isPresented
             _isSending = isSending
             super.init(nibName: nil, bundle: nil)
         }
@@ -58,14 +56,8 @@ extension MbModalHackView {
         }
         
         func presentationControllerDidAttemptToDismiss(_ presentationController: UIPresentationController) {
-//            print("presentationControllerDidAttemptToDismiss")
             
-            if isSending == true {
-                showingCancelSheet = false
-            }
-            else {
-                showingCancelSheet = true
-            }
+            isPresented = !isSending
         }
         
         // set delegate to the presentation of the root parent
@@ -73,10 +65,6 @@ extension MbModalHackView {
             guard let rootPresentationViewController = self.rootParent.presentationController, rootPresentationViewController.delegate == nil else { return }
             rootPresentationViewController.delegate = self
         }
-        
-//        override func viewDidDisappear(_ animated: Bool) {
-//
-//        }
     }
 }
 
@@ -91,53 +79,9 @@ extension UIViewController {
     }
 }
 
-/// make the call the SwiftUI style:
-/// view.allowAutDismiss(...)
 extension View {
-    /// Control if allow to dismiss the sheet by the user actions
-    
-    public func allowAutoDismiss(_ showingCancelSheet: Binding<Bool>, _ isSending: Binding<Bool>, _ dismissable: @escaping () -> Bool) -> some View {
+    public func allowAutoDismiss(_ isPresented: Binding<Bool>, _ isSending: Binding<Bool>, _ dismissable: @escaping () -> Bool) -> some View {
         self
-            .background(MbModalHackView(dismissable: dismissable, showingCancelSheet: showingCancelSheet, isSending: isSending))
+            .background(MbModalHackView(dismissable: dismissable, isPresented: isPresented, isSending: isSending))
     }
 }
-
-// MARK: - Example
-//struct ContentView: View {
-//    @State private var presenting = false
-//
-//    var body: some View {
-//        VStack {
-//            Button {
-//                presenting = true
-//            } label: {
-//                Text("Present")
-//            }
-//        }
-//        .sheet(isPresented: $presenting) {
-//            ModalContent()
-////                .allowAutoDismiss { false }     or
-////                 .allowAutoDismiss(false)
-//                .allowAutoDismiss($presenting, .constant(false)) {
-//                    false                   // true: can swipe down sheet, false: can't swipe down sheet
-//                }
-//        }
-//    }
-//}
-//
-//struct ModalContent: View {
-//    @Environment(\.presentationMode) private var presentationMode
-//
-//    var body: some View {
-//        VStack {
-//            Text("Hello")
-//                .padding()
-//
-//            Button {
-//                presentationMode.wrappedValue.dismiss()
-//            } label: {
-//                Text("Dismiss")
-//            }
-//        }
-//    }
-//}
